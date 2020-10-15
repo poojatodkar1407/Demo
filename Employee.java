9240774ad4f62d2fa1470f60527ba4defb6034c5
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-15 11:39:46 +0530

    [Pooja]Refactored: Chnaged in Sout

--- Employee.java
+++ Employee.java
@@ -4,7 +4,7 @@ Public class Employee{
 	psvm(String[] args){
 		sout("Welcome");
 		if(random == 1){		
-			sout("Employee is present");		
+			sout("Employee is present for the day");		
 `		}else{
 			int sum = a+c;
 }
